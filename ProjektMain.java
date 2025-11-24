457872c0a11e2625f8fdece54abcd567505f7606
Moritz <mraschk1@htl-steyr.ac.at>
2025-11-24 13:30:39 +0100

    Main tell call mr

--- ProjektMain.java
+++ ProjektMain.java
@@ -2,5 +2,8 @@ class ProjektMain{
     public static void main(String[] args){
         lkohberg lk = new lkohberg();
         lk.tell();
+
+        Mraschk1 mr = new Mraschk1();
+        mr.tell();
     }
 }
